3ddfe64ab13ae325d2fa217748b704d1bf06ed23
Mathieu Chauvin <45309030+mathieu-chauvin@users.noreply.github.com>
2022-08-11 18:13:47 +0700

    add solana wallet adapter

---
+++ ts-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Wallet } from './Wallet';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <Wallet />
+      
+    </div>
+  );
+}
+
+export default App;
